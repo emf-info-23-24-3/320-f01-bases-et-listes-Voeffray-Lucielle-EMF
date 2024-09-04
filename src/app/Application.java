3a160eb36f53c204a8570e5dbc6d4e946253acf9
Voeffray-Lucielle-EMF <lucielle.voeffray@studentfr.ch>
2024-09-04 09:22:06 +0200

    V1

--- src/app/Application.java
+++ src/app/Application.java
@@ -1,26 +1,28 @@
 package app;
 
+import java.util.ArrayList;
+
+import models.Personne;
+
 public class Application {
 
     public static void main(String[] args) {
 
-        //  --------------------------------------------------        
-        //           __     _____ _____ ____  _____ 
-        //           \ \   / / _ \_   _|  _ \| ____|
-        //            \ \ / / | | || | | |_) |  _|  
-        //             \ V /| |_| || | |  _ <| |___ 
-        //              \_/__\___/_|_|_|_| \_\_____|
-        //                / ___/ _ \|  _ \| ____|   
-        //               | |  | | | | | | |  _|     
-        //               | |__| |_| | |_| | |___    
-        //                \____\___/|____/|_____|   
-        //                    |_ _/ ___|_ _|        
-        //                     | | |    | |         
-        //                     | | |___ | |         
-        //                    |___\____|___|        
-        //                                                  
-        //  ---------------------------------------------------
+        ArrayList<Personne> personnes = new ArrayList<Personne>();
 
+        Personne adam = new Personne("Dizant", "Adam", "Impasse Satanique 35", "Les enfers", 666);
+        Personne gizel = new Personne("de la Fontaine", "gizel", "Impasse Satanique 35", "Les enfers", 666);
+        Personne alucard = new Personne("le Vampire", "alucard", "Impasse Satanique 35", "Les enfers", 666);
+        Personne logi = new Personne("Tech", "logi", "Impasse Satanique 35", "Les enfers", 666);
+
+        personnes.add(gizel);
+        personnes.add(adam);
+        personnes.add(alucard);
+        personnes.add(logi);
+
+        for (Personne personne : personnes) {
+            System.out.println(personne.getNom());
+        }
     }
 
 }
